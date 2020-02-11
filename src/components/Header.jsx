a70ec9593dc73c7c71d2ad561d7b87f0d3c2169c
Joshua Forman <cyborgforman@gmail.com>
2020-02-11 06:36:25 -0800

    update components

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header(){
+  var stylez = {
+    backgroundColor: 'black',
+    color: 'white'
+  }
+
+  return (
+    <div style={stylez}>
+      <h1>Help Queue</h1>
+    </div>
+  );
+}
+
+export default Header;
